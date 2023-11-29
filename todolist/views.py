476c79ee3a3ff313a93ced46eb6134903a51efea
San <andreal87@mail.ru>
2023-11-29 16:33:06 +0600

    add all func

--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
 from todolist.models import ToDo
 
 # Create your views here.
@@ -8,3 +10,24 @@ from todolist.models import ToDo
 def index(request):
     todos = ToDo.objects.all()
     return render(request, 'todoapp/index.html', {'todo_list': todos, 'title': 'Главная страница'})
+
+
+@require_http_methods(['POST'])
+def add(request):
+    title = request.POST['title']
+    todo = ToDo(title=title)
+    todo.save()
+    return redirect('index')
+
+
+def update(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.is_complete = not todo.is_complete
+    todo.save()
+    return redirect('index')
+
+
+def delete(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('index')
